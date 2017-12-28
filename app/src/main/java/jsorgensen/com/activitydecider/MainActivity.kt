baf088c2692125b37fb2db6ef99ad12e72ec9b58
Mike Jorgensen <mikeljorgensen@gmail.com>
2017-12-28 02:12:46 -0700

    finished things

--- app/src/main/java/jsorgensen/com/activitydecider/MainActivity.kt
+++ app/src/main/java/jsorgensen/com/activitydecider/MainActivity.kt
@@ -2,13 +2,27 @@ package jsorgensen.com.activitydecider
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val activityList = arrayListOf("Go to the Park", "Color", "Make a Snack", "Watch a Show", "Go for a Walk")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        decide
+        decideActivityButton.setOnClickListener {
+            val random = Random()
+            val randomActivity = random.nextInt(activityList.count())
+            selectedActivityText.text = activityList[randomActivity]
+        }
+
+        addActivityButton.setOnClickListener {
+            val newActivity = addActivityText.text.toString()
+            activityList.add(newActivity)
+            addActivityText.text.clear()
+        }
     }
 }
